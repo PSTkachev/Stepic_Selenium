0d573c35823d4a438ceda9ae6c0dcbb7ac00df72
PSTkachev <P.S.Tkachev@gmail.com>
2020-12-30 19:14:42 +0300

    Pytest (фикстура)

---
+++ abs.py
@@ -0,0 +1,8 @@
+def test_abs1():
+    assert abs(-42) == 42, "Should be absolute value of a number"
+
+def test_abs2():
+    assert abs(-42) == -42, "Should be absolute value of a number"
+
+def test_abs3():
+    assert abs(-2) == 2, "Should be absolute value of a number"
